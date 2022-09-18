ba83707260af36d3672c43c3b87dc3d33854de69
Ricardo Skewes <rskewes@college.harvard.edu>
2022-09-18 16:53:25 -0400

    Listo

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
-// Al cargar página web
-let gmHandler = new handler();
-gmHandler.crearBloques();
 
 
-
-
-function crearFila(institucion, bloque, rutaImg) {
-    let tablaSorteo = document.getElementById("tabla-sorteo");
-    var target = document.getElementById("ID");
-
-}
+const boton_sorteo = document.getElementById("boton_sorteo");
 
 
 // Un bloque define a un conjunto de cosas a rifar
@@ -28,17 +19,20 @@ class bloque {
 // handler a cargo de la lógica
 class handler {
     constructor() {
-        this.instituciones = ['Universidad de Guadalajara (UdeG)',
-                            'Instituto Tecnológico y de Estudios Superiores de Monterrey campus Guadalajara (TEC)', 
-                            'Instituto de Educación Superior de Occidente (ITESO)', 
-                            'Universidad Autónoma de Guadalajara (UAG)', 
-                            'Universidad del Valle de Atemajac (UNIVA)', 
-                            'Universidad Panamericana (UP)', 
-                            'Universidad Politécnica de la Zona Metropolitana de Guadalajara', 
-                            'Universidad Tecnológica de Jalisco',
-                            'Universidad Tecnológica de la Zona Metropolitana de Guadalajara',
-                            'Instituto Tecnológico José Mario Molina Pasquel y Henríquez.'];
+        this.instituciones = ['01. Universidad de Guadalajara (UdeG)',
+                            '02. Instituto Tecnológico y de Estudios Superiores de Monterrey campus Guadalajara (TEC)', 
+                            '03. Instituto de Educación Superior de Occidente (ITESO)', 
+                            '04. Universidad Autónoma de Guadalajara (UAG)', 
+                            '05. Universidad del Valle de Atemajac (UNIVA)', 
+                            '06. Universidad Panamericana (UP)', 
+                            '07. Universidad Politécnica de la Zona Metropolitana de Guadalajara', 
+                            '08. Universidad Tecnológica de Jalisco',
+                            '09. Universidad Tecnológica de la Zona Metropolitana de Guadalajara',
+                            '10. Instituto Tecnológico José Mario Molina Pasquel y Henríquez.'];
         this.bloques = [];
+        this.etapa = 0; // 0 = "previo al sorteo, 1 = sorteo de instituciones, 2 sorteo de bloques"
+        this.bloques_sorteados = 0;
+
     }
 
     // Dado un array, revuelve sus contenidos
@@ -59,12 +53,59 @@ class handler {
     }
 
     // Revuelve instituciones y bloques para repartir
-    iniciarRifa() {
-        this.instituciones = this.revolver(this.instituciones)
-        this.bloques = this.revolver(this.bloques);
-        for (let i = 0; i < this.instituciones.length; i++) {
-            console.log("Institución: " + this.instituciones[i]);
-            console.log("Sacó: " + this.bloques[i].getID())
+    sorteo() {
+        console.log("La etapa es: "+  this.etapa);
+        switch (this.etapa) {
+            case 0:
+                // Imprimir instituciones en orden
+                this.instituciones.forEach(institucion => {
+                    gmHandler.crearFila(institucion);
+                });
+                // Transicionar a siguiente etapa
+                boton_sorteo.innerHTML = "Sorteo de turnos";
+                this.etapa++;
+                break;
+            case 1:
+                // Revolver instituciones e imprimir
+                this.instituciones = this.revolver(this.instituciones);
+                this.borrarFilas();
+                this.instituciones.forEach(institucion => {
+                    gmHandler.crearFila(institucion);
+                });
+                //Transicionar a siguiente etapa (y rifar bloques)
+                boton_sorteo.innerHTML = "Rifar bloque";
+                this.bloques = this.revolver(this.bloques);
+                this.etapa++;
+                break;
+            case 2:
+                // Mostrar uno por uno los resultados hasta que se acaben
+                this.borrarFilas();
+                    this.bloques_sorteados++;
+                    
+                    // Imprimir completo los sorteados
+                    for (let i = 0; i < this.bloques_sorteados; i++) {
+                        this.crearFila(
+                            this.instituciones[i],
+                            this.bloques[i].id,
+                            this.bloques[i].img
+                        );
+                    }
+                    // Imprimir solo la institución de los no sorteados
+                    for (let i = this.bloques_sorteados; i < this.instituciones.length; i++) {
+                        this.crearFila(
+                            this.instituciones[i]
+                        );
+                    }
+                    // Transición hacia el fin del sorteo
+                    if (this.bloques_sorteados == this.instituciones.length){
+                        boton_sorteo.innerHTML = "Fin del sorteo";
+                        this.bloques_sorteados++;;
+                        
+                    }
+                    break;
+            default:
+                boton_sorteo.innerHTML = "Fin del sorteo";
+                
         }
     }
 
@@ -75,7 +116,42 @@ class handler {
             this.bloques.push(new bloque(index, "img" + index + ".PNG"));
         }
     }
+
+    crearFila(institucion, bloque, img_filename) {
+    // Preparar elementos
+        let tabla = document.getElementById("tabla-sorteo")
+        let fila = document.createElement("tr");
+        let dato1 = document.createElement("td");
+        dato1.innerHTML = institucion;
+        let dato2 = document.createElement('td');
+
+        // En caso de que se pase más de un argumento
+        if (bloque != undefined || img_filename != undefined){
+            let link = document.createElement('a');
+            link.href = "./img/" + img_filename;
+            link.target = "_blank";
+            link.innerHTML = bloque;
+            dato2.appendChild(link)
+        }
+
+        // Agregar elementos al DOM
+        fila.appendChild(dato1);
+        fila.appendChild(dato2);
+        tabla.appendChild(fila);
+    }
+
+    borrarFilas (){
+        let tabla = document.getElementById("tabla-sorteo");
+        while (tabla.firstChild) {
+            tabla.removeChild(tabla.firstChild);
+        }
+    }
+
+
 }
 
+// Al cargar página web
+let gmHandler = new handler();
+gmHandler.crearBloques();
 
-
+//gmHandler.crearFila("HARVARD", 1, "img1.PNG");
